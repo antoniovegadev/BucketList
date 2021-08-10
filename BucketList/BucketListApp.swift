207d3bf32250eb60e5d9db8e7383b48cee92adf4
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-10 11:10:02 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Antonio Vega on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
