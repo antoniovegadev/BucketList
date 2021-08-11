8151446a0daadeda15c6965e4158f4a4469bd176
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-11 02:43:03 -0500

    Downloading data from Wikipedia

---
+++ BucketList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by Antonio Vega on 8/11/21.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
